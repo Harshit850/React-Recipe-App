355e5c50cf6eb3ba09bd39cc98a8b005279c9f51
Harshit Kumar <kumarharshit850@gmail.com>
2021-01-05 00:52:22 +0530

    API working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Recipe from './Recipe';
 import './App.css'
 
 const axios = require('axios');
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
   const APP_ID = "6dbfa5af";
   const APP_KEY = "d061b1c78111fc24e3b8148e3bfd25c9";
-  
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const exampleReq = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const handleClick = () => {
     axios.get(exampleReq)
       .then(function (response) {
-      // handle success
-      let recipe = [];
-      recipe = response.data.hits[0].recipe.ingredientLines;
-      console.log(recipe);
+      setRecipes(response.data.hits)
     })
     .catch(function (error) {
-      // handle error
       console.log(error);
     });
   }
 
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
-    <div>
-      <form className="search-form">
-        <input className="search-bar" type="text" required/>
+    <div className="app">
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={handleSearch} required/>
         <button onClick={handleClick} className="search-btn" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+          key={Math.random()*1000+1}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   )
 }
